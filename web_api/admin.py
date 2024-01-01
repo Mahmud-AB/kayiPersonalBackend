35b4eabe327e4b5000e75fa9dae9a5d8ac35505d
nirban007 <nirbanmitra007@gmail.com>
2024-01-02 00:23:12 +0600

    app registered successfuly to the backend

--- web_api/admin.py
+++ web_api/admin.py
@@ -1,7 +1,9 @@
-# admin.py
-
 from django.contrib import admin
-from .models import Users
+from .models import Users, UsersForgotCode, HomePageItems, PushNotification, AuditLog
 
-
+# Register your models here.
 admin.site.register(Users)
+admin.site.register(UsersForgotCode)
+admin.site.register(HomePageItems)
+admin.site.register(PushNotification)
+admin.site.register(AuditLog)
